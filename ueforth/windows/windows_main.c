0b88254955d9c62658132718f765fc82309af035
Brad Nelson <flagxor@gmail.com>
2021-01-23 09:50:18 -0800

    Fixing web

--- ueforth/windows/windows_main.c
+++ ueforth/windows/windows_main.c
@@ -4,9 +4,13 @@
 
 #define CALLTYPE WINAPI
 #if defined(_M_X64)
-# define SSMOD_FUNC --sp; cell_t b, a = _mul128(*sp, sp[1], &b); tos = _div128(b, a, tos, sp)
+# define SSMOD_FUNC \
+  --sp; cell_t b, a = _mul128(*sp, sp[1], &b); b = _div128(b, a, tos, sp); \
+  if (*sp < 0) { *sp += tos; tos = b - 1; } else { tos = b; }
 #elif defined(_M_IX86)
-# define SSMOD_FUNC --sp; __int64 a = (__int64) *sp * (__int64) sp[1]; tos = _div64(a, tos, sp)
+# define SSMOD_FUNC \
+  --sp; __int64 a = (__int64) *sp * (__int64) sp[1]; cell_t b = _div64(a, tos, sp); \
+  if (*sp < 0) { *sp += tos; tos = b - 1; } else { tos = b; }
 #endif
 
 #include "common/opcodes.h"
