864cd930b7cb239a53e0c701b29ab1e8088a09d8
Brad Nelson <flagxor@gmail.com>
2021-06-28 22:23:21 -0700

    Add license under Apache 2.0.
    Add copyright + license notice to all files.
    Enhance scripts to work around / drop license text when composing
    multiple file.
    Drop historical EForth reference.
    Bump version number.

--- ueforth/esp32/fuse_ino.js
+++ ueforth/esp32/fuse_ino.js
@@ -1,15 +1,43 @@
 #! /usr/bin/env nodejs
+// Copyright 2021 Bradley D. Nelson
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 var fs = require('fs');
+
+function DropCopyright(source) {
+  var lines = source.split('\n');
+  var cleaned = [];
+  for (var i = 0; i < lines.length; ++i) {
+    if (lines[i].search('Copyright') >= 0) {
+      while (lines[i] != '') {
+        ++i;
+      }
+    } else {
+      cleaned.push(lines[i]);
+    }
+  }
+  return cleaned.join('\n');
+}
 
 var version = process.argv[2];
 var revision = process.argv[3];
 var code = fs.readFileSync(process.argv[4]).toString();
-var opcodes = fs.readFileSync(process.argv[5]).toString();
-var calling = fs.readFileSync(process.argv[6]).toString();
-var core = fs.readFileSync(process.argv[7]).toString();
-var interp = fs.readFileSync(process.argv[8]).toString();
-var boot = fs.readFileSync(process.argv[9]).toString();
+var opcodes = DropCopyright(fs.readFileSync(process.argv[5]).toString());
+var calling = DropCopyright(fs.readFileSync(process.argv[6]).toString());
+var core = DropCopyright(fs.readFileSync(process.argv[7]).toString());
+var interp = DropCopyright(fs.readFileSync(process.argv[8]).toString());
+var boot = DropCopyright(fs.readFileSync(process.argv[9]).toString());
 
 code = code.replace('{{VERSION}}', function() { return version; });
 code = code.replace('{{REVISION}}', function() { return revision; });
--- ueforth/posix/terminal.fs
+++ ueforth/posix/terminal.fs
@@ -1,3 +1,17 @@
+\ Copyright 2021 Bradley D. Nelson
+\
+\ Licensed under the Apache License, Version 2.0 (the "License");
+\ you may not use this file except in compliance with the License.
+\ You may obtain a copy of the License at
+\
+\     http://www.apache.org/licenses/LICENSE-2.0
+\
+\ Unless required by applicable law or agreed to in writing, software
+\ distributed under the License is distributed on an "AS IS" BASIS,
+\ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+\ See the License for the specific language governing permissions and
+\ limitations under the License.
+
 also posix also termios
 s" /dev/ttyS3" r/w O_NONBLOCK or open-file throw constant remote
 256 constant ICRNL
