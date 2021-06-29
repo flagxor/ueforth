5779238dd578370a6ae7cdee630de0eed4501977
Brad Nelson <flagxor@gmail.com>
2021-06-28 22:23:21 -0700

    Add license under Apache 2.0.
    Add copyright + license notice to all files.
    Enhance scripts to work around / drop license text when composing
    multiple file.
    Drop historical EForth reference.
    Bump version number.

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
 EForth is a delightfully minimalist approach to Forth originated by Bill Muench and Dr. C. H. Ting.
 
-For more information visit:
+For documentation visit:
 
 [https://eforth.appspot.com/](https://eforth.appspot.com/)
 
 ## Directories
 
-* ueforth/ - µEforth - EForth refactored to allow source boostraping.
-* circleforth/ - A minimalist Forth modeled after toy Lisps.
-* reference/ - EForth Reference Material
+* [ueforth/ - µEforth / ESP32forth](ueforth/README.md) - EForth refactored to allow source boostraping.
+* [circleforth/ - CircleForth](circleforth/README.md) - A minimalist Forth modeled after toy Lisps.
--- ueforth/common/boot.fs
+++ ueforth/common/boot.fs
@@ -1,4 +1,19 @@
 : (   41 parse drop drop ; immediate
+: \   10 parse drop drop ; immediate
+
+\ Copyright 2021 Bradley D. Nelson
+\
+\ Licensed under the Apache License, Version 2.0 (the "License");
+\ you may not use this file except in compliance with the License.
+\ You may obtain a copy of the License at
+\
+\     http://www.apache.org/licenses/LICENSE-2.0
+\
+\ Unless required by applicable law or agreed to in writing, software
+\ distributed under the License is distributed on an "AS IS" BASIS,
+\ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+\ See the License for the specific language governing permissions and
+\ limitations under the License.
 
 ( Useful Basic Compound Words )
 : nip ( a b -- b ) swap drop ;
@@ -25,9 +40,6 @@
 : 2* 2 * ;   : 2/ 2 / ;
 : 4* 4 * ;   : 4/ 4 / ;
 : +! ( n a -- ) swap over @ + swap ! ;
-
-( Line Comments )
-: \   nl parse drop drop ; immediate
 
 ( Cells )
 : cell+ ( n -- n ) cell + ;
