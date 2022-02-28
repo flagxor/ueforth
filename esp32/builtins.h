11e666e9a17e9ca76605557cda248d4008d5449e
Brad Nelson <flagxor@gmail.com>
2022-02-27 21:45:28 -0800

    Adding Serial2.

--- esp32/builtins.h
+++ esp32/builtins.h
@@ -89,7 +89,13 @@ static cell_t ResizeFile(cell_t fd, cell_t size);
   XV(serial, "Serial.available", SERIAL_AVAILABLE, PUSH Serial.available()) \
   XV(serial, "Serial.readBytes", SERIAL_READ_BYTES, n0 = Serial.readBytes(b1, n0); NIP) \
   XV(serial, "Serial.write", SERIAL_WRITE, n0 = Serial.write(b1, n0); NIP) \
-  XV(serial, "Serial.flush", SERIAL_FLUSH, Serial.flush())
+  XV(serial, "Serial.flush", SERIAL_FLUSH, Serial.flush()) \
+  XV(serial, "Serial2.begin", SERIAL2_BEGIN, Serial2.begin(tos); DROP) \
+  XV(serial, "Serial2.end", SERIAL2_END, Serial2.end()) \
+  XV(serial, "Serial2.available", SERIAL2_AVAILABLE, PUSH Serial2.available()) \
+  XV(serial, "Serial2.readBytes", SERIAL2_READ_BYTES, n0 = Serial2.readBytes(b1, n0); NIP) \
+  XV(serial, "Serial2.write", SERIAL2_WRITE, n0 = Serial2.write(b1, n0); NIP) \
+  XV(serial, "Serial2.flush", SERIAL2_FLUSH, Serial2.flush())
 
 #define REQUIRED_ARDUINO_GPIO_SUPPORT \
   Y(pinMode, pinMode(n1, n0); DROPn(2)) \
