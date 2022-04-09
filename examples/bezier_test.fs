1ae27f72cf30e4538f58cca7f1f434542d71af5b
Brad Nelson <flagxor@gmail.com>
2022-04-09 07:30:35 -0700

    Fix windows dependency in example.

--- examples/bezier_test.fs
+++ examples/bezier_test.fs
@@ -14,7 +14,7 @@
 \ See the License for the specific language governing permissions and
 \ limitations under the License.
 
-windows dpi-aware
+DEFINED? windows [IF] windows dpi-aware [THEN]
 also graphics
 also structures
 
